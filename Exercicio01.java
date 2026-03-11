37f164f6df5a6d87dce4f4f527c6c67a14778b68
LUANA SILVA AZEVEDO <luana.azevedo@univille.br>
2026-03-11 20:23:17 -0300

    feat:Exercicio01

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,4 +1,6 @@
 //Faça um programa concorrente que crie 10 threads que exibam o nome da thread e a hora atual.
+/* import java.util.Date;
+
 public class Exercicio01 extends Thread {
     public void run(){
         System.out.printf("%s: %tT\n", getName(), new Date());
@@ -14,3 +16,26 @@ public class Exercicio01 extends Thread {
         }
     }
 }
+*/
+
+import java.util.Date;
+
+public class Exercicio01 implements Runnable {
+
+    public void run(){
+        System.out.printf("%s: %tT\n", Thread.currentThread().getName(), new Date());
+    }
+
+    public static void main(String[] args) {
+
+        Thread[] threads = new Thread[10];
+        Exercicio01 tarefa = new Exercicio01();
+
+        for(int i = 0; i < 10; i++){
+            threads[i] = new Thread(tarefa);
+            threads[i].start();
+        }
+    }
+}
+
+
