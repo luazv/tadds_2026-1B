bcdb1a5ada6f5df9ec2396ee9b9970e64ce54c92
LUANA SILVA AZEVEDO <luana.azevedo@univille.br>
2026-03-11 21:56:14 -0300

    feat:Exercicio01

--- Exercicio03.java
+++ Exercicio03.java
@@ -1,13 +1,5 @@
-/*Faça um programa que gere um array de 1 bilhão de números inteiros aleatórios e faça as seguintes tarefas:
+/*Faça um programa que gere um array de 1 bilhão de números inteiros aleatórios e faça as seguintes tarefas:*/
 
-Some todos os valores de forma sequencial e meça o tempo de execução;
-Some todos os valores de forma concorrente com 10 threads e meça o tempo de execução;
-Some todos os valores de forma concorrente com 100 threads e meça o tempo de execução.
-Responda as seguintes perguntas:
-Qual foi o speedup1 obtido?
-Teve algum caso que o speedup foi negativo?
-Repita os testes acima usando Threads virtuais.
-Houve diferenças entre Threads de plataforma e Threads virtuais? Se houve, quais foram e explique o porquê das diferenças*/
 
 import java.util.Random;
 
@@ -107,3 +99,8 @@ public class Exercicio03 {
         System.out.printf("Tempo: %d ms\n\n", System.currentTimeMillis() - te);
     }
 }
+/*Responda as seguintes perguntas:
+Qual foi o speedup1 obtido?
+Teve algum caso que o speedup foi negativo?
+Repita os testes acima usando Threads virtuais.
+Houve diferenças entre Threads de plataforma e Threads virtuais? Se houve, quais foram e explique o porquê das diferenças*/
